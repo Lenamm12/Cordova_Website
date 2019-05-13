2241f743b96445f99bcac233243919bb92434c08
Lenamm12 <45099435+Lenamm12@users.noreply.github.com>
2019-05-13 13:12:30 +0200

    weiße navbar

--- Cordova_Versuch3/.bin/node.cmd
+++ Cordova_Versuch3/.bin/node.cmd
@@ -1,6 +1,5 @@
 @echo off
 SET PATH=%PATH%;%~dp0
-SET HTTP_PROXY=http://1:1@127.0.0.1:8888
+    SET HTTP_PROXY=http://1:1@127.0.0.1:8888
     SET HTTPS_PROXY=http://1:1@127.0.0.1:8888
 "%~dp0..\..\packages\Node.js.0.10.28\node.exe" %*
-  
